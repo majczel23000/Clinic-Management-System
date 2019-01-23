29bd26dcc70226a3a921486f4db4a4ff6dfe585b
danielpupczyk <pupczyk.daniel4@gmail.com>
2019-01-23 13:10:59 +0100

    lista wizyt

--- frontend/src/app/main/visits/visits.component.ts
+++ frontend/src/app/main/visits/visits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visits',
@@ -7,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisitsComponent implements OnInit {
 
-  constructor() { }
+  visits: any[] = [
+    {
+      date: '2018-03-10',
+      description: 'opis 1',
+      _id: '123456789'
+    },
+    {
+      date: '2018-03-11',
+      description: 'opis 2',
+      _id: '12332524564'
+    },
+    {
+      date: '2018-03-12',
+      description: 'opis 3',
+      _id: '342524354'
+    },
+    {
+      date: '2018-04-10',
+      description: 'opis 4',
+      _id: '12345adf45'
+    }
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  
+  detailsVisit(index: number){
+    this.router.navigate(['/dashboard/visits/details'], {queryParams: {'id': this.visits[index]._id}});
+  }
 
 }
